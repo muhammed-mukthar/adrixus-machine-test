686bd630891e0ad3ac32ed00c6af6ecdf22bfce1
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-12-21 18:18:33 +0530

    final bug fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const homeRouter=require('./routes/home')
 dotenv.config()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: process.env.CORS_VARS.split(", ") }));
  mongoose.connect(process.env.DBURL)
  .then(()=>console.log('connected to db')
  )
